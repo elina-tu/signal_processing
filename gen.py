33614c56cda5f146a96ccd1de865221148092f64
Elina <tubiselina@gmail.com>
2020-10-13 17:37:54 +0100

    slider for number of points to plot, making code neater in callback functions

--- gen.py
+++ gen.py
@@ -15,29 +15,36 @@ def closeCallback(event):
 
 def freqCallback(value):
     '''change frequency according to slider'''
-    plotHandle.set_ydata(sine(value, float(phaseHandle.val), \
-                                           time(float(timeHandle.val), num)))
+    plotHandle.set_ydata(sine(value, phaseHandle.val, time(timeHandle.val, \
+                                                           int(numHandle.val))))
+    plotHandle.set_xdata(time(timeHandle.val, int(numHandle.val)))
     ax.relim()
     ax.autoscale_view()
     plt.draw()
 
 def timeCallback(value):
     '''change time interval over which function is plotted'''
-    plotHandle.set_ydata(sine(float(freqHandle.val), float(phaseHandle.val),\
-                                                           time(value, num)))
-    plotHandle.set_xdata(time(value, num))
+    plotHandle.set_ydata(sine(freqHandle.val, phaseHandle.val,time(value, \
+                                                            int(numHandle.val))))
+    plotHandle.set_xdata(time(value, int(numHandle.val)))
     ax.relim()
     ax.autoscale_view()
     plt.draw()
 
 def phaseCallback(value):
     '''change phase of the function'''
-    plotHandle.set_ydata(sine(float(freqHandle.val), value, time(float(timeHandle.val),\
-                                                             num)))
+    plotHandle.set_ydata(sine(freqHandle.val, value, time(timeHandle.val, \
+                                                          int(numHandle.val))))
     ax.relim()
     ax.autoscale_view()
     plt.draw()
     
+def numCallback(value):
+    '''change number of points'''
+    plotHandle.set_ydata(sine(freqHandle.val, phaseHandle.val, time(timeHandle.val,\
+                                                                    int(value))))
+    plotHandle.set_xdata(time(timeHandle.val, int(value)))
+    plt.draw()
     
 fig = plt.figure(figsize=(10, 6))
 
@@ -74,7 +81,13 @@ timeHandle.on_changed(timeCallback)
 
 #phase slider
 sax2 = plt.axes([0.75, 0.4, 0.1, 0.03])
-phaseHandle = widgets.Slider(sax2, 'phase', valmin=0, valmax=2*np.pi, valinit=0)
+phaseHandle = widgets.Slider(sax2, 'phase', valmin=0, valmax=2*np.pi, valinit=phase)
 phaseHandle.on_changed(phaseCallback)
+
+#number of points slider
+sax3 = plt.axes([0.75, 0.3, 0.1, 0.03])
+numHandle = widgets.Slider(sax3, 'sample number', valmin=2, valmax=500, valinit=num, \
+                           valfmt='%d', valstep=1)
+numHandle.on_changed(numCallback)
 
 plt.show()
