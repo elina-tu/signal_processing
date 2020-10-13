6b40d6d19cc8b7d106e752d9453e991b66a97e77
Elina <tubiselina@gmail.com>
2020-10-13 14:27:15 +0100

    create slider to change frequency

--- gen.py
+++ gen.py
@@ -13,14 +13,21 @@ def closeCallback(event):
     '''"off" button callback to close the window'''
     plt.close()
 
+def freqCallback(val):
+    '''change frequency according to slider'''
+    plotHandle.set_ydata(sine(val, t))
+    plt.draw()
+
 t = 1 #s
 num = 100 #number of points
 freq = 1 #Hz
 t = np.linspace(0, t, num)
-sine = np.sin(2*np.pi*freq*t)
+
+#function for wave
+sine = lambda freq, t : np.sin(2*np.pi*freq*t)
 
 ax = plt. axes([0.15, 0.1, 0.6, 0.8])
-plt.plot(t, sine, 'k-')
+plotHandle, = plt.plot(t, sine(freq, t), 'k-')
 plt.xlabel('t, s')
 plt.ylabel('f, Hz')
 
@@ -28,5 +35,11 @@ plt.ylabel('f, Hz')
 bax = plt.axes([0.8, 0.2, 0.1, 0.05])
 buttonHandle = widgets.Button(bax, 'Off')
 buttonHandle.on_clicked(closeCallback)
+
+#frequency slider
+sax = plt.axes([0.92, 0.5, 0.03, 0.4])
+freqHandle = widgets.Slider(sax, 'frequency', valmin=1, valmax=10, valinit=0.5,\
+                            orientation='vertical')
+freqHandle.on_changed(freqCallback)
 
 plt.show()
