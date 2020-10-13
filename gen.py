ef5d8c097cd80556e5c38959dd768cd9a1659abb
Elina <tubiselina@gmail.com>
2020-10-13 16:16:07 +0100

    change slider position, set window size

--- gen.py
+++ gen.py
@@ -19,12 +19,15 @@ def freqCallback(value):
     plt.draw()
 
 def timeCallback(value):
+    '''change time interval over which function is plotted'''
     plotHandle.set_ydata(sine(float(freqHandle.val), time(value, num)))
     plotHandle.set_xdata(time(value, num))
     ax.relim()
     ax.autoscale_view()
     plt.draw()
     pass
+
+fig = plt.figure(figsize=(10, 6))
 
 t = 1 #s
 num = 100 #number of points
@@ -35,24 +38,24 @@ time = lambda t, num: np.linspace(0, t, num)
 #function for wave
 sine = lambda freq, t : np.sin(2*np.pi*freq*t)
 
-ax = plt.axes([0.15, 0.1, 0.5, 0.8])
+ax = plt.axes([0.1, 0.1, 0.5, 0.8])
 plotHandle, = plt.plot(time(t, num), sine(freq, time(t, num)), 'k-')
 plt.xlabel('t, s')
 plt.ylabel('f, Hz')
 
 #off button
-bax = plt.axes([0.8, 0.1, 0.1, 0.05])
+bax = plt.axes([0.75, 0.1, 0.1, 0.05])
 buttonHandle = widgets.Button(bax, 'Off')
 buttonHandle.on_clicked(closeCallback)
 
 #frequency slider
-sax = plt.axes([0.92, 0.5, 0.03, 0.4])
+sax = plt.axes([0.92, 0.5, 0.02, 0.4])
 freqHandle = widgets.Slider(sax, 'frequency', valmin=1, valmax=10, valinit=1,\
                             orientation='vertical')
 freqHandle.on_changed(freqCallback)
 
 #time slider
-sax1 = plt.axes([0.7, 0.5, 0.1, 0.03])
+sax1 = plt.axes([0.75, 0.5, 0.1, 0.03])
 timeHandle = widgets.Slider(sax1, 'time interval', valmin=0.1, valmax=10, valinit=t)
 timeHandle.on_changed(timeCallback)
 
